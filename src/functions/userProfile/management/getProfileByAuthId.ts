5605952fdcd36d55e5e3aa86d8eb6250d653e46c
Owen Barton <owen17101@msn.com>
2022-01-10 11:07:58 -0500

    Update getProfileByAuthId response object

--- src/functions/userProfile/management/getProfileByAuthId.ts
+++ src/functions/userProfile/management/getProfileByAuthId.ts
@@ -1,4 +1,5 @@
 import { getClient } from "../../../contentful/client"
+import UserProfile from "../../../types/UserProfile";
 
 export async function main(event: any) {
 
@@ -16,8 +17,25 @@ export async function main(event: any) {
         if (usersEntriesResponse.total == 0) {
             return null;
         }
+
+        const entry = usersEntriesResponse.items[0];
+        const fields = (entry.fields as any);
+
+        const profile : UserProfile = {
+            id: entry.sys.id,
+            authId: fields.id ? fields.id['en-US'] : "",
+            name: fields.name ? fields.name['en-US']: "",
+            slug: fields.slug,
+            photo: null,
+            bio: fields.bio ? fields.bio['en-US'] : "",
+            attributes: fields.attributes ? fields.attributes['en-US'].map((attribute: any) => {
+                return {
+                    id: attribute.sys.id,
+                }
+            }): []
+        }
         
-        return usersEntriesResponse.items[0];
+        return profile;
     } catch (error) {
         console.error(error);    
 
